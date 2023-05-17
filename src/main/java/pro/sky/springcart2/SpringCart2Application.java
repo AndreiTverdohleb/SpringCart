386d94c72dfc7b0db517f4c004840766d18e8330
Andrei Tverdohleb <tverdo10@mail.ru>
2023-05-17 18:12:35 +0300

    Начальный коммит!

---
+++ src/main/java/pro/sky/springcart2/SpringCart2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.springcart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/springcart2/SpringCart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springcart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
